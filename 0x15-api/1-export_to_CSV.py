6876c2ce0386cd37a9e0ba1b564833737a36736c
Albert Mpepo <albertmpepo@gmail.com>
2024-01-03 01:33:08 +0200

    1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Export to CSV"""
+
+import requests
+import sys
+import csv
+
+url = "https://jsonplaceholder.typicode.com/"
+
+
+def fetch_employee_progress(employee_id):
+    """
+    given employee ID, returns information about his/her TODO list progress.
+
+    Extend your Python script to export data in the CSV format.
+        Records all tasks that are owned by this employee
+        Format must be: "USER_ID","USERNAME",
+                        "TASK_COMPLETED_STATUS","TASK_TITLE"
+        File name must be: USER_ID.csv
+    """
+    base_url = url + "users/{}".format(employee_id)
+    todos_url = url + "todos?userId={}".format(employee_id)
+
+    user_response = requests.get(base_url)
+    user_data = user_response.json()
+    name = user_data.get("name")
+
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    with open(f"{employee_id}.csv", "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for task in todos_data:
+            writer.writerow(
+                [employee_id, name, task.get("completed"), task.get("title")]
+            )
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    fetch_employee_progress(employee_id)
