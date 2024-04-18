0f6349d8b219377509db0853637ad8ac95b3fc76
Albert Mpepo <albertmpepo@gmail.com>
2024-04-18 15:15:30 +0200

    refrac: Task on debugging complete

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Increase the traffic limit to fix failed request
+# Increase the traffic limit to fix failed request.
 
-# Increase on the ULIMIT of the default file
+# Increase on the ULIMIT of the default file.
 exec { 'Nginx-fix':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
