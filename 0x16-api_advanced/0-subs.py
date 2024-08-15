aa2be32018d69d7372545734ec277c62beba88b1
Albert Mpepo <albertmpepo@gmail.com>
2024-08-15 14:44:21 +0200

    PEP8 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,6 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-
     if response.status_code == 200:
         data = response.json()
         return str(data['data']['subscribers'])
